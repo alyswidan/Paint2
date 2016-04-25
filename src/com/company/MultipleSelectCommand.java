0d5de180b3b9318e83756ac3ee9a56a1415dfbdb
alyswidan <swidan12340@gmail.com>
2016-04-25 20:33:31 +0200

    hi

--- src/com/company/MultipleSelectCommand.java
+++ src/com/company/MultipleSelectCommand.java
@@ -1,9 +1,17 @@
 package com.company;
 
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+
 /**
  * Created by ADMIN on 4/25/2016.
  */
 public class MultipleSelectCommand implements Command{
+    EventHandler<MouseEvent> multipleSelectHandler;
+
+    public MultipleSelectCommand() {
+
+    }
 
     @Override
     public void execute() {
