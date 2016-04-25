791333c3be8eacc35f329d5aef5161dc62805426
alyswidan <swidan12340@gmail.com>
2016-04-25 20:25:01 +0200

    hi

--- src/com/company/MultipleSelectCommand.java
+++ src/com/company/MultipleSelectCommand.java
@@ -3,9 +3,15 @@ package com.company;
 /**
  * Created by ADMIN on 4/25/2016.
  */
-public class MultipleSelectCommand {
+public class MultipleSelectCommand implements Command{
 
+    @Override
+    public void execute() {
 
+    }
 
+    @Override
+    public void undo() {
 
+    }
 }
