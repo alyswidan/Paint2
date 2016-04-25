b0510eeba2793e2b64a76e57046ece655b95366d
alyswidan <swidan12340@gmail.com>
2016-04-25 19:09:56 +0200

    hi

---
+++ src/com/company/moveCommand.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import javafx.scene.shape.Shape;
+import javafx.scene.transform.Translate;
+
+/**
+ * Created by ADMIN on 4/25/2016.
+ */
+public class MoveCommand implements Command {
+    private Selection selection;
+    private Translate translate;
+
+    public MoveCommand(Selection selection,double x,double y) {
+        this.selection = selection;
+        translate = new Translate(x,y);
+    }
+
+    @Override
+    public void execute() {
+        selection.addTransform(translate);
+    }
+
+    @Override
+    public void undo() {
+        selection.addTransform(translate.createInverse());
+    }
+}
