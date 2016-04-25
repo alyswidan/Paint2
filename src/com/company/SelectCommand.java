320aed1469ce97a918e53ba4b7f20f7262c9c800
alyswidan <swidan12340@gmail.com>
2016-04-25 15:48:50 +0200

    hi

--- src/com/company/SelectCommand.java
+++ src/com/company/SelectCommand.java
@@ -1,8 +1,25 @@
 package com.company;
 
+import javafx.scene.shape.Shape;
+
 /**
  * Created by ADMIN on 4/25/2016.
  */
-public class SelectCommand {
+public class SelectCommand implements Command{
+    Shape shape;
+    public SelectCommand(Shape shape)
+    {
+        this.shape = shape;
+    }
+
+    @Override
+    public void execute() {
+        SelectionManager.getInstance().add(Selection.fromBounds(shape.getBoundsInLocal()));
+    }
+
+    @Override
+    public void undo() {
+        SelectionManager.getInstance().remove(Selection.fromBounds(shape.getBoundsInLocal()));
+    }
 
 }
