35af5d6264865000a1ff9a5cc00ffb91f85f35b3
alyswidan <swidan12340@gmail.com>
2016-04-28 22:27:57 +0200

    hi

--- src/com/company/PasteCommand.java
+++ src/com/company/PasteCommand.java
@@ -17,6 +17,7 @@ public class PasteCommand implements Command{
 
     @Override
     public void execute() {
+
         DrawingCanvas.getInstance().getCanvas().getChildren().addAll(selection.getShapes());
     }
 
